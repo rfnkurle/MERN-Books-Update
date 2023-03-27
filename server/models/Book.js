f02a5a3348ca5ac33fed9e6507490bbedfc76b83
rfnkurle <45407665+rfnkurle@users.noreply.github.com>
2023-03-27 12:21:42 -0600

    models folder. User model and book schema

---
+++ server/models/Book.js
@@ -0,0 +1,31 @@
+const { Schema } = require('mongoose');
+
+// subdocument schema
+const bookSchema = new Schema({
+  authors: [
+    {
+      type: String,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+
+  bookId: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = bookSchema;
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const User = require('./User');
+
+//export User model with book subdocument
+module.exports = { User };
