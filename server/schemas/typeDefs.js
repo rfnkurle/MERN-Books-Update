efd2778a766158e3518f88ea02fbf55177a7d41d
rfnkurle <45407665+rfnkurle@users.noreply.github.com>
2023-03-27 12:46:07 -0600

    basic query and mutation schemas in typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,7 +23,25 @@ const typeDefs = gql`
     user: User
   }
 
-  
+  input bookInput {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String, password: String): Auth
+    addUser(username: String, email: String, password: String): Auth
+    saveBook(bookData: bookInput): User
+    removeBook(bookId: ID): User
+  }
 `;
 
 module.exports = typeDefs;
